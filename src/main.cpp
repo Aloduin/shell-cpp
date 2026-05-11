99147ce1cc25171fe52d100d81bd92040a97d151
Aloduin <aloduin@qq.com>
2026-05-11 20:56:01 +0800

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,15 @@ int main() {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  // TODO: Uncomment the code below to pass the first stage
-  std::cout << "$ ";
+  while (true) {
+    // TODO: Uncomment the code below to pass the first stage
+    std::cout << "$ ";
 
-  // Captures the user's command in the "command" variable
-  std::string command;
-  std::getline(std::cin, command);
+    // Captures the user's command in the "command" variable
+    std::string command;
+    std::getline(std::cin, command);
 
-  // Prints the {command}: command not found
-  std::cout << command << ": command not found" << std::endl;
-
+    // Prints the {command}: command not found
+    std::cout << command << ": command not found" << std::endl;
+  }
 }
